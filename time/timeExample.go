3d538eb902c896210333e4f1823600aeebb30ece
preston <taopopoo@126.com>
2015-10-15 17:29:04 +0800

    add time example

--- time/timeExample.go
+++ time/timeExample.go
@@ -13,7 +13,7 @@ func main() {
 	// time.Sleep(time.Second * 3)
 
 	// eTest5()
-	heartbeat()
+	eTest7()
 }
 
 /*
@@ -25,6 +25,7 @@ func heartbeat() {
 	}
 }
 
+//在一定时间之后做某个事情
 func eTest2() {
 	fmt.Println("the 1")
 	tc := time.After(time.Second)
@@ -57,4 +58,25 @@ func eTest5() {
 	t := time.Now()
 	yesterday := time.Unix(t.Unix()-(60*60*24), int64(t.Nanosecond()))
 	fmt.Println(yesterday)
+}
+
+//计算一段时间相隔秒数
+func eTest6() {
+	t := time.Now()
+	time.Sleep(time.Second)
+	interval := time.Now().Sub(t)
+	done := interval > 1
+	fmt.Println("---", done)
+
+}
+
+/*
+	计算当天半夜12点
+*/
+func eTest7() {
+	str := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Println(str[:10])
+
+	newTime, _ := time.Parse("2006-01-02 15:04:05", str[:10]+" 00:00:00")
+	fmt.Println(newTime.Add(time.Hour * 24))
 }
